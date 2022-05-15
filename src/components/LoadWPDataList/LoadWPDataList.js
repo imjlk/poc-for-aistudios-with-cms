9c687dfce6919d6da920cf0cbfeb849a972cd61c
imjlk <i@m.junglei.kim>
2022-05-15 21:53:56 +0900

    스타일 수정

--- src/components/LoadWPDataList/LoadWPDataList.js
+++ src/components/LoadWPDataList/LoadWPDataList.js
@@ -1,56 +1,74 @@
 import { client } from 'client';
 import LoadMore from 'components/LoadMore';
-import useNodePagination, { POST_NODES_PREPASS_FIELDS } from 'hooks/useNodePagination';
+import useNodePagination, {
+  POST_NODES_PREPASS_FIELDS,
+} from 'hooks/useNodePagination';
 import React, { useState } from 'react';
 
 import styles from './LoadWPDataList.module.scss';
 
-
-
-function SelectedItem({ slug }) {
-  const { useQuery } = client
-  const selectedPost = useQuery().post({
-    id: slug,
-    idType: 'SLUG',
-  })
-
-  return <>
-    <div>Selected Title: {selectedPost?.title()}</div>
-    <div>Contents:
-      <textarea name="" id="" cols="30" rows="10" value={selectedPost.content()}></textarea>
-    </div>
-  </>;
-}
-
 export default function LoadWPDataList() {
-  const [selectedItemSlug, setSelectedItemSlug] = useState('')
+  const [selectedItemSlug, setSelectedItemSlug] = useState('');
   const { data, fetchMore, isLoading } = useNodePagination(
-    (query, queryArgs) => query.posts(queryArgs),
+    (query, queryArgs) => query.projects(queryArgs),
     POST_NODES_PREPASS_FIELDS
   );
 
-  return <div className={styles.wrapper}>
-    <ul>
-      {data?.nodes.map((node, i) => (
-        <li key={i}>
-          <a href="#" onClick={(e) => {
-            e.preventDefault();
-            setSelectedItemSlug(node?.slug)
-          }}>
+  return (
+    <div className={styles.wrapper}>
+      <ul>
+        {data?.nodes.map((node, i) => (
+          <li key={i}>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                setSelectedItemSlug(node?.slug);
+              }}
+            >
+              {node?.title()}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <LoadMore
+        className="text-center"
+        hasNextPage={data?.hasNextPage}
+        endCursor={data?.endCursor}
+        isLoading={isLoading}
+        fetchMore={fetchMore}
+      />
 
-            {node?.title()}
-          </a>
-        </li>
-      ))}
-    </ul>
-    <LoadMore
-      className="text-center"
-      hasNextPage={data?.hasNextPage}
-      endCursor={data?.endCursor}
-      isLoading={isLoading}
-      fetchMore={fetchMore}
-    />
+      {selectedItemSlug && <SelectedItem slug={selectedItemSlug} />}
+    </div>
+  );
+}
 
-    {selectedItemSlug && <SelectedItem slug={selectedItemSlug} />}
-  </div>
+function SelectedItem({ slug }) {
+  const { useQuery } = client;
+  const selectedPost = useQuery().project({
+    id: slug,
+    idType: 'SLUG',
+  });
+
+  return (
+    <>
+      <div>Selected Title: {selectedPost?.title()}</div>
+      <div>
+        <label htmlFor="">Preview:</label>
+        <div
+          className={styles.reset}
+          dangerouslySetInnerHTML={{ __html: selectedPost?.contentArea }}
+        ></div>
+      </div>
+      <div>
+        Contents:
+        <textarea
+          className={styles['origin-text']}
+          rows="10"
+          value={selectedPost.contentArea}
+        ></textarea>
+      </div>
+    </>
+  );
 }
