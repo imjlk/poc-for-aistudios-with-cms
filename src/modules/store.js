09e0e798a407ecf28f10eee6a2a5e0306d5c75b2
imjlk <i@m.junglei.kim>
2022-05-16 15:55:30 +0900

    Redux Toolkit 설정으로 변경

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,20 +1,21 @@
-import { createStore, applyMiddleware } from "redux";
-import { createWrapper } from "next-redux-wrapper";
-import { composeWithDevTools } from "redux-devtools-extension";
-import createMiddleware from "redux-saga";
+import { configureStore } from '@reduxjs/toolkit';
+import { createWrapper } from 'next-redux-wrapper';
+import createSagaMiddleware from 'redux-saga';
 
-import rootReducer from "./reducers";
-import rootSaga from "./sagas";
+import rootReducer from './reducers';
+import rootSaga from './sagas';
 
-const sagaMiddleware = createMiddleware();
+const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(sagaMiddleware))
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [sagaMiddleware],
+  devTools: process.env.NODE_ENV !== 'production',
+});
 
 sagaMiddleware.run(rootSaga);
 
+// next-redux-wrapper 때문?
 const makeStore = () => store;
 
 export const wrapper = createWrapper(makeStore);
