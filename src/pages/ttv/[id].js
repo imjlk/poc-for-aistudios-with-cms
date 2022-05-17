02c467b53b8268bbcaf85d79c87743edf06b549a
imjlk <i@m.junglei.kim>
2022-05-17 18:59:38 +0900

    TTV상세 조회 페이지 수정

--- src/pages/ttv/[id].js
+++ src/pages/ttv/[id].js
@@ -1,3 +1,96 @@
+import { client } from 'client';
+import {
+  SEO,
+  EntryHeader,
+  Header,
+  Footer,
+  Main,
+  SubNav,
+  AuthWrapper,
+} from 'components';
+import { pageTitle } from 'utils';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+// import { useSelector } from 'react-redux';
+import { getNextServerSideProps } from '@faustjs/next';
+
 export default function Page() {
-  return <div>TTV Detail</div>;
+  const { useQuery } = client;
+  const generalSettings = useQuery().generalSettings;
+  const router = useRouter();
+  const { id } = router.query;
+  const [currentData, setCurrentData] = useState({});
+  // console.log('query', id, ttv);
+
+  // dispatch(fetchTTV())
+  useEffect(() => {
+    (async () => {
+      const {
+        data: { data },
+      } = await axios.get(`/api/ttv/${id}`);
+      setCurrentData(data);
+      console.log(data);
+    })();
+    // console.log(ttv)
+  }, []);
+  return (
+    <>
+      <SEO title={`상세 조회 - ${pageTitle(generalSettings)}`} />
+
+      <Header />
+      <Main>
+        <AuthWrapper>
+          <SubNav />
+          <EntryHeader title="상세 조회" />
+          <div className="container">
+            {Object.entries(currentData).map(([key, value], index) => (
+              <div key={index}>
+                <label>{key}</label>
+                <p>{value}</p>
+                {key === 'videoUrl' && (
+                  <div
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      right: 0,
+                      width: '33%',
+                    }}
+                  >
+                    <video controls>
+                      {/* <source src={aistudios.video} /> */}
+                      <source src={value} />
+                      해당 브라우저는 video 태그를 지원하지 않습니다.
+                    </video>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </AuthWrapper>
+      </Main>
+
+      <Footer />
+    </>
+  );
 }
+
+export async function getServerSideProps(context) {
+  return getNextServerSideProps(context, {
+    Page,
+    client,
+  });
+}
+
+// export async getNextServerSideProps(ctx) {
+//   return getNextStaticProps(ctx, {
+//     Page,
+//     client,
+//   });
+// }
+// export async function getStaticProps(context) {
+//   return getNextStaticProps(context, {
+//     Page,
+//     client,
+//   });
+// }
